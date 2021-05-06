acc111b50d6a5ba23f878d25084ec68acb43b34b
ngranback <nella.granback@gmail.com>
2021-05-06 12:57:45 -0500

    working PyPollgit status! but needs double checking

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -38,7 +38,7 @@ with open(inpath, 'r') as CSVin:
     #skip header
     next(electionData, None)
 
-
+    #identify each candidate as a Key and tally votes as Values
     for row in electionData:
         totalVotes += 1
         
@@ -46,11 +46,33 @@ with open(inpath, 'r') as CSVin:
             candidateDict[row[2]] += 1           
         else:
             candidateDict[row[2]] = 1 
-      
+
     
+#identify key with largest number as the value
+#aka identify candidate with most votes
+voteList = candidateDict.values()
+mostVotes = max(voteList)
 
-print(candidateDict)
-print(totalVotes)
+for key, value in candidateDict.items():
+    if value == mostVotes:
+        winner = key #winner of the election!!
+        
+
+#assign percentages to each candidate in the dictionary    
+print('Election Results\n===================\n(Candidate, Percentage, Votes Received)')
 
 
+for key in candidateDict:
+    percentage = candidateDict[key]/totalVotes
+    
+    print(key, '---', '{0:.0f}%'.format(percentage*100), '---',  candidateDict[key])
+    
+print(f'===================\nThe winner is {winner}!!!')
 
+
+print
+
+    
+  
+    
+ 
